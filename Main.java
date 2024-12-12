1072c2b35486e57ae72b6d648b78466dfee900ba
polurumanasa3 <manasap142@gmail.com>
2024-12-11 21:23:25 -0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+interface printable{
+
+}
+
+class a implements printable{
+    public void print()
+    {
+        System.out.println("Hello students");
+    }
+}
+public class Main
+{
+    public static void main(String args[])
+    {
+        a f=new a();
+        f.print();
+    }
+}
+
+    
+
+    
+
